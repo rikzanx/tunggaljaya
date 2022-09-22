9f7e94aebc992362524d8c0f48947ecde4970849
rikzanx <erik.kasbam@gmail.com>
2022-09-22 13:12:21 +0700

    fix css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -27,7 +27,7 @@ https://templatemo.com/tm-559-zay-shop
 </head>
 
 <body>
-    <a href="https://api.whatsapp.com/send?phone=51955081075&text=Hola%21%20Quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20Varela%202." class="float-ok" target="_blank">
+    <a href="https://api.whatsapp.com/send/?phone={{ $company->telp }}&text=Halo%20{{ $company->name }}">{{ $company->telp }}" class="float-ok" target="_blank">
         <i class="fa fa-whatsapp my-float-ok"></i>
         </a>
     <!-- Start Top Nav -->
