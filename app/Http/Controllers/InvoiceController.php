4b52d39496a5f0c6cbb4228099954ca025729975
rikzanx <erik.kasbam@gmail.com>
2023-09-26 11:37:56 +0700

    okee

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -22,7 +22,7 @@ class InvoiceController extends Controller
      */
     public function index()
     {
-        $invoices = Invoice::withSum('items','total')->with('items')->get();
+        $invoices = Invoice::withSum('items','total')->with('items.total')->get();
         return view('admin.invoice',[
             'invoices' => $invoices,
         ]);
