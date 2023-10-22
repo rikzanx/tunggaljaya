04a76a969d0113dd2957819a6dc210f0d4415248
Muhammad Rikzan <30933800+rikzanx@users.noreply.github.com>
2023-10-22 12:57:14 +0700

    fssf

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -234,7 +234,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js" integrity="sha512-pHVGpX7F/27yZ0ISY+VVjyULApbDlD0/X0rgGbTqCE7WFW5MezNTWG/dnhtbBuICzsd0WQPgpE4REBLv+UqChw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script>
 $(document).ready(function() {
-    $('.formatted-number').mask('0,000', {reverse: true});
+    $('.formatted-number').mask('0.fsf000', {reverse: true});
 });
 </script>
 
