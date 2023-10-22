89fcdfdb22715b147993c4aac72f0002760d200e
Muhammad Rikzan <30933800+rikzanx@users.noreply.github.com>
2023-10-22 15:00:00 +0700

    fs

--- resources/views/admin/deleteditem.blade.php
+++ resources/views/admin/deleteditem.blade.php
@@ -7,8 +7,8 @@
     <section class="content-header">
       <div class="container-fluid">
         <div class="row mb-2">
-          {<div class="col-sm-6">
-            <h1>{ config('app.name', 'Laravel') }}Deleted Items</h1>
+          <div class="col-sm-6">
+            <h1>{{ config('app.name', 'Laravel') }}Deleted Items</h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
