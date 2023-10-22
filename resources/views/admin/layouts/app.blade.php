faf95ac7139f6d17dc27359e6aff3f01882c429c
Muhammad Rikzan <30933800+rikzanx@users.noreply.github.com>
2023-10-22 12:54:50 +0700

    fs cdn

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -231,8 +231,7 @@
 <script src="{{ asset('plugins/bs-custom-file-input/bs-custom-file-input.min.js') }}"></script>
 <!-- Ekko Lightbox -->
 <script src="{{ asset('plugins/ekko-lightbox/ekko-lightbox.min.js') }}"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-mask/1.14.18/jquery.mask.min.js"></script>
-
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js" integrity="sha512-pHVGpX7F/27yZ0ISY+VVjyULApbDlD0/X0rgGbTqCE7WFW5MezNTWG/dnhtbBuICzsd0WQPgpE4REBLv+UqChw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script>
 $(document).ready(function() {
     $('.formatted-number').mask('0,000', {reverse: true});
